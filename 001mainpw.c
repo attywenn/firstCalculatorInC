43f5ee243102d67f80e7b0603d3342f235c2feb7
attywenn <wnciplays@gmail.com>
2025-03-22 14:53:56 +0700

    firstCalculatorC.c

--- 001mainpw.c
+++ 001mainpw.c
@@ -1,73 +1,74 @@
 #include <stdio.h>
 
 double a, b;
-int value;
+int mathIntDivide (int a, int b);
+int mathRemainder (int a, int b);
 double mathAdd (double a, double b);
 double mathSubtract (double a, double b);
 double mathMultiply (double a, double b);
 double mathDivide (double a, double b);
-int mathIntDivide (int a, int b);
-int mathRemainder (int a, int b);
 
 int main () {
-	char choice;
-	
-	a = 5;
-	b = 10;
-	value = a, b;
+    char choice;
 
-		do {
-			printf("Input first number: ");
-			scanf(" %lf", &a);
-			printf("Input second number: ");
-			scanf(" %lf", &b);
-			
-			printf("Pick your operator (+, -, *, /): ");
-			scanf(" %c", &choice);
+    do {
+        printf("Input first number: ");
+        scanf("%lf", &a);
+        printf("Input second number: ");
+        scanf("%lf", &b);
 
-			switch (choice) {
-				case '+':
-					printf("Value: %.2lf\n", mathAdd(a, b));
-					mathAdd(a, b);
-					break;
-				case '-':
-					printf("Value: %.2lf\n", mathSubtract(a,b));
-					mathSubtract(a, b);
-					break;
-				case '*':
-					printf("Value: %.2lf\n", mathMultiply(a, b));
-					mathMultiply(a, b);
-					break;
-				case '/':
-					printf("Value: %.2lf\vValue: %d r. %d\n", mathDivide(a, b), mathIntDivide(a, b), mathRemainder(a, b));
-					mathDivide(a, b);
-					mathRemainder(a, b);
-					break;
-			} 
-		} while (choice != '+' && '-' && '*' && '/');
-	return 0;	
+        printf("Pick your operator (+, -, *, /): ");
+        scanf(" %c", &choice);
+
+        switch (choice) {
+            case '+':
+                printf("Value: %.2lf\n", mathAdd(a, b));
+                break;
+            case '-':
+                printf("Value: %.2lf\n", mathSubtract(a, b));
+                break;
+            case '*':
+                printf("Value: %.2lf\n", mathMultiply(a, b));
+                break;
+            case '/':
+                if (b != 0) {
+                    printf("Value: %.2lf\n", mathDivide(a, b));
+                    printf("Integer Division: %d\n", mathIntDivide((int)a, (int)b));
+                    printf("Remainder: %d\n", mathRemainder((int)a, (int)b));
+                } else {
+                    printf("Error: Division by zero is not allowed.\n");
+                }
+                break;
+            default:
+                printf("Invalid operator. Please enter one of +, -, *, /.\n");
+                break;
+        }
+        // To end the loop, the user must choose an operator that is not +, -, *, or /
+    } while (choice == '+' || choice == '-' || choice == '*' || choice == '/');
+
+    return 0;
 }
 
 double mathAdd (double a, double b) {
-	return a + b;
+    return a + b;
 }
 
 double mathSubtract (double a, double b) {
-	return a - b;
+    return a - b;
 }
 
 double mathMultiply (double a, double b) {
-	return a * b;
+    return a * b;
 }
 
 double mathDivide (double a, double b) {
-	return a / b;
-}
-
-int mathRemainder (int a, int b) {
-	return a % b;
+    return a / b;
 }
 
 int mathIntDivide (int a, int b) {
-	return a / b;
+    return a / b;
+}
+
+int mathRemainder (int a, int b) {
+    return a % b;
 }
